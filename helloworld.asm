6b0a1e178dceeb54548da548643237c04ea9a215
billyrayvalentine <ben@cerium.org>
2020-11-01 19:09:55 +0000

    fix: Working with 24x24

--- helloworld.asm
+++ helloworld.asm
@@ -28,7 +28,7 @@ cpu_entrypoint:
     * Load the palette into CRAM
     move.l  #0xC0000000, VDP_CTRL_PORT
 
-    lea     Palette0, a0
+    lea     BRPalette0, a0
     moveq   #16-1, d0
 
 1:  move.w  (a0)+, VDP_DATA_PORT
@@ -38,53 +38,84 @@ cpu_entrypoint:
     * Skip the first 32 bytes of VRAM so we have a blank tile
     move.l  #0x40200000, VDP_CTRL_PORT
 
-    lea     TilesLetters, a0
-    moveq   #56-1, d0
-
-1:  move.l  (a0)+, VDP_DATA_PORT
-    dbra    d0, 1b
-
     * Load the bar tiles
-    lea     TilesBar, a0
-    moveq   #32-1, d0
+    lea     BRImage0, a0
+    moveq   #72-1, d0
 
 1:  move.l  (a0)+, VDP_DATA_PORT
     dbra    d0, 1b
 
-    * Update plane A table @ 0xC000
-    move.l  #0x40000003, VDP_CTRL_PORT
-
-    move.w  #0x001, VDP_DATA_PORT
-    move.w  #0x002, VDP_DATA_PORT
-    move.w  #0x003, VDP_DATA_PORT
-    move.w  #0x003, VDP_DATA_PORT
-    move.w  #0x004, VDP_DATA_PORT
-    move.w  #0x000, VDP_DATA_PORT
 
     * Update plane table B @ 0xE000
     move.l  #0x60000003, VDP_CTRL_PORT
 
-    move.w  #0x005, VDP_DATA_PORT
-    move.w  #0x004, VDP_DATA_PORT
-    move.w  #0x006, VDP_DATA_PORT
+    move.w  #0x001, VDP_DATA_PORT
+    move.w  #0x002, VDP_DATA_PORT
     move.w  #0x003, VDP_DATA_PORT
-    move.w  #0x007, VDP_DATA_PORT
-
-    * Load initial sprite table to 0xAB00
-    move.l  #0x68000002, VDP_CTRL_PORT
-    lea     BarSprite, a0
-    move.l  (a0)+, VDP_DATA_PORT
-    move.l  (a0)+, VDP_DATA_PORT
-
-    * Set the initial X and Y positions
-    move.w  #128, RAM_SPRITE_TARGET_X
-    move.w  #128, RAM_SPRITE_TARGET_Y
-
-    move.w  #128, d6 /* Counter for x  */
-    move.w  #128, d7 /* Counter for y  */
-
-    move.l  #0, d4 /* Counter for x scroll offset */
-    move.l  #0, d5 /* Counter for y scroll offset */
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x000, VDP_DATA_PORT
+    move.w  #0x004, VDP_DATA_PORT
+    move.w  #0x005, VDP_DATA_PORT
+    move.w  #0x006, VDP_DATA_PORT
 
 /*
  * Main loop
@@ -98,78 +129,6 @@ cpu_entrypoint:
 forever:
 
     jsr wait_vblank_start
-    jsr read_controller_1
-
-    * If right then move sprite right
-    btst.b  #3, RAM_CONTROLLER_1
-    beq 1f
-
-    * Check the sprite is at the screen boundary, if so skip
-    * Screen h
-    cmpi.w  #320 + 128 - 32, RAM_SPRITE_TARGET_X
-    beq 4f
-
-    addi.w  #1, RAM_SPRITE_TARGET_X
-    addi.w  #1, d6
-
-    * If left then move sprite left
-1:  btst.b  #2, RAM_CONTROLLER_1
-    beq 2f
-
-    * Check the sprite is at the screen boundary, if so skip
-    cmpi.w  #0 + 128, RAM_SPRITE_TARGET_X
-    beq 4f
-
-    subi.w  #1, RAM_SPRITE_TARGET_X
-    subi.w  #1, d6
-
-
-    * If up then move the sprite up
-2:  btst.b  #0, RAM_CONTROLLER_1
-    beq 3f
-
-    * check the sprite is at the screen boundary, if so skip
-    cmpi.w  #0 + 128, RAM_SPRITE_TARGET_Y
-    beq 4f
-
-    subi.w  #1, RAM_SPRITE_TARGET_Y
-    subi.w  #1, d7
-
-
-    * If down then move the sprite down
-3:  btst.b  #1, RAM_CONTROLLER_1
-    beq 4f
-
-    * Check the sprite is at the screen boundary, if so skip
-    cmpi.w  #224 + 128 - 8, RAM_SPRITE_TARGET_Y
-    beq 4f
-
-    addi.w  #1, RAM_SPRITE_TARGET_Y
-    addi.w  #1, d7
-
-
-    * Update horizontal scroll table @ 0xAC00 and scroll plane A
-    * If the offset is >320 (size of screen width) then reset to 0
-4:  cmp.w   #320, d4
-    bls.s   1f
-    move.w  #0, d4
-
-1:  move.l  #0x6C000002, VDP_CTRL_PORT
-    move.w  d4, VDP_DATA_PORT
-
-    * Update vertical scroll table which is in VSRAM
-    * skip the first word as we want to scroll plane B
-    cmp.w   #224, d5
-    bls.s   1f
-    move.w  #0, d5
-
-1:  move.l  #0x40020010, VDP_CTRL_PORT
-    move.w  d5, VDP_DATA_PORT
-
-    add.w  #1, d4
-    add.b  #1, d5
-
-    jsr update_sprite_table
     jsr wait_vblank_end
     jmp forever
 
@@ -228,6 +187,7 @@ wait_vblank_end:
 .include "init_vdp.asm"
 .include "tmss.asm"
 .include "palletes.asm"
+.include "brhead24.asm"
 
 /*
  * Interrupt handler
